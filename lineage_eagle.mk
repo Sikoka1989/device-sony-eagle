c4882d073cebb7a1e88dd0e7edd71ca499bd7727
Sikoka1989 <44112337+Sikoka1989@users.noreply.github.com>
2019-03-12 16:04:27 +0300

    Update lineage_eagle.mk

--- lineage_eagle.mk
+++ lineage_eagle.mk
@@ -2,8 +2,6 @@
 TARGET_SCREEN_HEIGHT	:= 960
 TARGET_SCREEN_WIDTH	:= 540
 
-TARGET_KERNEL_CONFIG := aosp_yukon_eagle_defconfig
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/sony/eagle/device.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
@@ -13,10 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Fingerprint for eagle (from stock)
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=D2303 \
-    PRIVATE_BUILD_DESC="D2303-user 5.1.1 18.6.A.0.182 1807889774 release-keys"
-
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D2303
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D2303-user 5.1.1 18.6.A.0.182 1807889774 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += 
 BUILD_FINGERPRINT := Sony/D2303/D2303:5.1.1/18.6.A.0.182/1807889774:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-sonyericsson
